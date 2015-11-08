752950642a716dceddd102fc776dcc67203c9c94
dmitry <zrayev@mail.ru>
2015-11-08 19:15:48 +0200

    Add create categoty to install.php

--- bin/install.php
+++ bin/install.php
@@ -4,7 +4,9 @@ require_once __DIR__ . '/../config/autoload.php';
 
 use Layer\Connector\ConnectorClass;
 use Layer\Manager\PostManager;
+use Layer\Manager\CategoryManager;
 use Entity\Post;
+use Entity\Category;
 
 $connector = new ConnectorClass($config['db_name'], $config['db_user'], $config['db_password']);
 $connector ->createDataBase($config['db_name'], $config['db_user'], $config['db_password'],$config['host']);
@@ -29,3 +31,12 @@ $post
 ;
 $postManager->insert($post);
 
+$categoryManager = new CategoryManager($connector);
+$categoryManager->createTable();
+$category = new Category();
+$category->setName('Категорія 1');
+$categoryManager->insert($category);
+
+$category = new Category();
+$category->setName('Категорія 2');
+$categoryManager->insert($category);
