ed83e04694b33c244bcbce33b81a8ff1cb70057f
dmitry <zrayev@mail.ru>
2015-11-09 00:11:52 +0200

    new file:   src/View/Post/create.html.twig 	new file:   src/View/Post/index.html.twig 	new file:   web/.htaccess 	modified:   web/index.php

--- web/index.php
+++ web/index.php
@@ -5,11 +5,40 @@ require_once __DIR__ . '/../config/autoload.php';
 use Layer\Connector\ConnectorClass;
 use Layer\Manager\CategoryManager;
 use Layer\Manager\PostManager;
+use Phroute\Phroute\RouteCollector;
+use Phroute\Phroute\Dispatcher;
+use Controllers\PostController;
 
 $connector = new ConnectorClass($config['db_name'], $config['db_user'], $config['db_password']);
 $categoryManager = new CategoryManager($connector);
 $postManager = new PostManager($connector);
 $postManager->setCategoryManager($categoryManager);
 
-$post = $postManager->find(1);
-var_dump($post);
+$loader = new \Twig_Loader_Filesystem(__DIR__ . '/../src/View');
+$twig = new \Twig_Environment($loader, [
+    'cache' => false,
+]);
+
+$router = new RouteCollector();
+$router->any('/', function() use($postManager, $categoryManager, $twig) {
+    $controller = new PostController($postManager, $categoryManager,$twig);
+
+    return $controller->indexAction();
+});
+
+$router->any('/show/{id}', function($id)  use($postManager, $categoryManager, $twig) {
+    $controller = new PostController($postManager, $categoryManager, $twig);
+
+    return $controller->showAction($id);
+});
+
+$router->any('/create', function()  use($postManager, $categoryManager, $twig) {
+    $controller = new PostController($postManager, $categoryManager, $twig);
+
+    return $controller->createAction();
+});
+
+$dispatcher = new Dispatcher($router->getData());
+$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+echo $response;
