4ff1e6e3b4955b6af699779de375ca36eb7b7e3b
dmitry <zrayev@mail.ru>
2015-11-08 19:13:04 +0200

    refactor managers

--- src/Layer/Manager/Manager.php
+++ src/Layer/Manager/Manager.php
@@ -15,6 +15,11 @@ abstract class Manager implements ManagerInterface
     protected $connector;
 
     /**
+     * @var string
+     */
+    protected $tableName;
+
+    /**
      * Manager constructor.
      * @param ConnectorClass $connector
      */
@@ -25,4 +30,88 @@ abstract class Manager implements ManagerInterface
 
     abstract protected function createObject(array $fields);
 
+    /**
+     * @param array $fields
+     * @return object[]
+     */
+    protected function createObjects(array $fields)
+    {
+        $objects = [];
+
+        foreach ($fields as $objectFields) {
+            $objects[] = $this->createObject($objectFields);
+        }
+
+        return $objects;
+    }
+
+    /**
+     * @param $id
+     * @return object|null
+     */
+    public function find($id)
+    {
+        $statement = $this->connector->connect()
+            ->prepare("SELECT * FROM {$this->tableName} WHERE id = :id LIMIT 1");
+        $statement->bindValue(':id', (int) $id);
+        $statement->execute();
+        $result = $statement->fetchAll();
+
+        if (count($result) === 1) {
+            return $this->createObject($result[0]);
+        }
+
+        return null;
+    }
+
+    /**
+     * @return object
+     */
+    public function findAll()
+    {
+        $statement = $this->connector->connect()
+            ->prepare("SELECT * FROM {$this->tableName}")
+        ;
+        $statement->execute();
+
+        return $this->createObject($statement->fetchAll());
+    }
+
+    /**
+     * @param array $criteria
+     * @return object
+     */
+    public function findBy($criteria = [])
+    {
+        $sql = "SELECT * FROM {$this->tableName} WHERE ";
+        $lastKey = end(array_keys($criteria));
+
+        foreach ($criteria as $fieldName => $fieldValue) {
+            $sql .= "$fieldName = $fieldValue";
+
+            if ($fieldName !== $lastKey) {
+                $sql .= ' AND ';
+            }
+        }
+
+        $statement = $this->connector->connect()
+            ->prepare($sql)
+        ;
+        $statement->execute();
+
+        return $this->createObject($statement->fetchAll());
+    }
+
+    /**
+     * @param object $post
+     * @return bool
+     */
+    public function remove($post)
+    {
+        $statement = $this->connector->connect()
+            ->prepare("DELETE FROM {$this->tableName} WHERE id = :id");
+        $statement->bindValue(':id', $post->getId(), \PDO::PARAM_INT);
+
+        return $statement->execute();
+    }
 }
--- src/Layer/Manager/PostManager.php
+++ src/Layer/Manager/PostManager.php
@@ -30,19 +30,6 @@ class PostManager extends Manager
      * @param Post $post
      * @return bool
      */
-    public function remove($post)
-    {
-        $statement = $this->connector->connect()
-            ->prepare("DELETE FROM  {$this->tableName} WHERE id = :id");
-        $statement->bindValue(':id', $post->getId(), \PDO::PARAM_INT);
-
-        return $statement->execute();
-    }
-
-    /**
-     * @param Post $post
-     * @return bool
-     */
     public function update($post)
     {
         $statement = $this->connector->connect()
@@ -56,59 +43,6 @@ class PostManager extends Manager
         $statement->bindValue(':body', $post->getBody());
 
         return $statement->execute();
-    }
-
-    /**
-     * @param $id
-     * @return Post|null
-     */
-    public function find($id)
-    {
-        $statement = $this->connector->connect()
-            ->prepare('SELECT * FROM Post WHERE id = :id LIMIT 1');
-        $statement->bindValue(':id', (int) $id);
-        $statement->execute();
-        $result = $statement->fetchAll();
-
-        if (count($result) === 1) {
-            return $this->createObject($result[0]);
-        }
-
-        return null;
-    }
-
-    /**
-     * @return Post
-     */
-    public function findAll()
-    {
-        $statement = $this->connector->connect()
-            ->prepare("SELECT * FROM {$this->tableName}")
-        ;
-        $statement->execute();
-
-        return $this->createObject($statement->fetchAll());
-    }
-
-    public function findBy($criteria = [])
-    {
-        $sql = "SELECT * FROM {$this->tableName} WHERE ";
-        $lastKey = end(array_keys($criteria));
-
-        foreach ($criteria as $fieldName => $fieldValue) {
-            $sql .= "$fieldName = $fieldValue";
-
-            if ($fieldName !== $lastKey) {
-                $sql .= ' AND ';
-            }
-        }
-
-        $statement = $this->connector->connect()
-            ->prepare($sql)
-        ;
-        $statement->execute();
-
-        return $this->createObject($statement->fetchAll());
     }
 
     /**
@@ -127,20 +61,7 @@ class PostManager extends Manager
         $statement->exec($sql);
     }
 
-    /**
-     * @param array $fields
-     * @return Post[]
-     */
-    protected function createObjects(array $fields)
-    {
-        $objects = [];
 
-        foreach ($fields as $objectFields) {
-            $objects[] = $this->createObject($objectFields);
-        }
-
-        return $objects;
-    }
 
     /**
      * @param array $fields
